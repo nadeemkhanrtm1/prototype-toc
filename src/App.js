6d39da492d1030fe5f960e4784d3d6ac6835bd3e
nadeemkhanrtm1 <nadeemkhanrtm@gmail.com>
2021-02-20 19:33:55 +0530

    add first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React,{ useEffect, useState } from 'react'
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+// @autobind
+const App = () => {
+  // const [data,setData] = useState('');
+  // const [renderr,setRenderr] = useState([]); 
+  const [arr,setArr] = useState([]);
+  
+  useEffect(()=>{
+    axios.get('https://wordpress.ferofly.com/wp-json/wp/v2/posts').then(data=>{
+
+      var dummyArray=[]
+data.data.map(item=>{
+      ReactHtmlParser(item.content.rendered).map(data=>{
+      if(data.type === "h2"){
+        // console.log(data)
+        // setRenderr(data)
+      if(data.props.children.length > 1 && data.props.children[0].type!="strong"){
+        // var toPush={
+        //   number:data.props.children[0],
+        //   information:data.props.children[1].props.children[0] 
+        // }
+        var totalString = `<h2 id="${data.props.children[1].props.children[0]}">${data.props.children[0]} ${data.props.children[1].props.children[0]}</h2>`
+        dummyArray.push(totalString)
+        
+        // setRenderr(data)
+        // setData(data)
+      }
+    }
+    })
+    ReactHtmlParser()
+    })
+    setArr(dummyArray)
+      // setData(data.data)
+    }).catch(err=>console.log(err))
+  },[])
+console.log(arr)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* {data && ReactHtmlParser(renderr) }  */}
+       
+       {arr?arr.map(item=>ReactHtmlParser(item)):null}
+      
     </div>
   );
 }
